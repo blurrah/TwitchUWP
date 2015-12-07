89b37a672cc518e714cb39ec551e7a55dc3f7fa5
blurrah <borisbesemer@gmail.com>
2015-12-07 13:22:04 +0100

    adds games model and networking code

--- TwitchUWP/TwitchAPIHelper.cs
+++ TwitchUWP/TwitchAPIHelper.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using TwitchUWP.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.IO;
 
 namespace TwitchUWP
 {
@@ -12,9 +15,25 @@ namespace TwitchUWP
         private async Task<String> CallTwitchAsync(string url)
         {
             HttpClient http = new HttpClient();
+            http.DefaultRequestHeaders.Add("Accept", "application/vnd.twitchtv.v3+json");
+
             var response = await http.GetAsync(url);
 
             return await response.Content.ReadAsStringAsync();
+        }
+
+        private static async Task<GameWrapper> GetTopGamesAsync()
+        {
+            string topGamesURL = "https://api.twitch.tv/kraken/games/top?limit=100";
+
+            string jsonMessage = await CallTwitchAsync(topGamesURL);
+
+            var serializer = new DataContractJsonSerializer(typeof(GameWrapper));
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonMessage));
+
+            var result = (GameWrapper)serializer.ReadObject(ms);
+
+            return result;
         }
     }
 }
