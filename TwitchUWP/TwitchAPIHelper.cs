16634669802c7799ab32d59583ac26e201ae7f3d
blurrah <borisbesemer@gmail.com>
2015-12-07 12:39:02 +0100

    adds a TwitchAPIHelper class

---
+++ TwitchUWP/TwitchAPIHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchUWP
+{
+    public class TwitchAPIHelper
+    {
+        private async Task<String> CallTwitchAsync(string url)
+        {
+            HttpClient http = new HttpClient();
+            var response = await http.GetAsync(url);
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
