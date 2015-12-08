752167b7e9d6a9a5f96bfc209f79b3c26057ebf1
Jasper Berghoef <jasper.berghoef@gmail.com>
2015-12-08 13:12:50 +0100

    Function for rendering text into chat view.

--- TwitchUWP/VideoPage.xaml.cs
+++ TwitchUWP/VideoPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using TwitchUWP.Models;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Text;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -34,6 +35,29 @@ namespace TwitchUWP
             this.InitializeComponent();
 
             chatMessages = new ObservableCollection<Message>();
+
+            RenderMessage("Jasper", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et quam id ex tempus varius non et mi.");
+        }
+
+        private void RenderMessage(string name, string content)
+        {
+            TextBlock tb = new TextBlock();
+            tb.Margin = new Thickness(0, 0, 0, 5);
+            tb.TextWrapping = TextWrapping.WrapWholeWords;
+
+            Run renderName = new Run();
+            renderName.Text = name + ": ";
+            renderName.FontWeight = FontWeights.Bold;
+            renderName.Foreground = new SolidColorBrush(Color.FromArgb(0xFF, 0x64, 0x41, 0xA5));
+
+            Run renderContent = new Run();
+            renderContent.Text = content;
+
+            tb.Inlines.Add(renderName);
+            tb.Inlines.Add(renderContent);
+
+            ChatMessageView.Children.Add(tb);
+            ChatScrollViewer.ChangeView(0.0f, ChatScrollViewer.ExtentHeight, 1.0f);
         }
     }
 }
