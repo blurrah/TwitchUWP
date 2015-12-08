ce2b888637e3336e6b265d32ffa87b96babcebe9
Jasper Berghoef <jasper.berghoef@gmail.com>
2015-12-08 11:08:42 +0100

    Created chat message model.

---
+++ TwitchUWP/VideoPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using TwitchUWP.Models;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TwitchUWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class VideoPage : Page
+    {
+
+        public ObservableCollection<Message> chatMessages { get; set; }
+
+        public VideoPage()
+        {
+            this.InitializeComponent();
+
+            chatMessages = new ObservableCollection<Message>();
+        }
+    }
+}
