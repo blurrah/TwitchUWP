179a665c2565790c44ccebc71853724c12baf936
Mitchell Slager <mslager92@gmail.com>
2015-12-08 15:25:35 +0100

    New structure and added back button + navigatio

--- TwitchUWP/GameOverviewPage.xaml.cs
+++ TwitchUWP/GameOverviewPage.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using TwitchUWP.Models;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +25,28 @@ namespace TwitchUWP
     /// </summary>
     public sealed partial class GameOverviewPage : Page
     {
+        public ObservableCollection<Game> topGames { get; set; }
+
         public GameOverviewPage()
         {
             this.InitializeComponent();
+
+            topGames = new ObservableCollection<Game>();
+            RefreshGames();
+        }
+
+        public async void RefreshGames()
+        {
+            Task t = TwitchAPIHelper.PopulateTwitchTopGamesAsync(topGames);
+            await t;
+        }
+
+        private void GamesListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var selectedGame = (Game)e.ClickedItem;
+            string gameTitle = selectedGame.name;
+
+            this.Frame.Navigate(typeof(DetailViewPage), gameTitle);
         }
     }
 }
